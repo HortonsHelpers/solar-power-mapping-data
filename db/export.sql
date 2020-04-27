8efdb10b126c65ec0bfc0c64b6b220567fef77b1
Dan Stowell <danstowell@users.sourceforge.net>
2020-04-27 18:19:28 +0100

    Create SQL commands to produce CSV data snapshots

---
+++ db/export.sql
@@ -0,0 +1,34 @@
+/*
+** Solar PV database export to CSV etc
+** April 2020
+** Author: Dan Stowell
+*/
+
+-- "Suggested REPD IDs for OSM"
+CREATE TEMP TABLE "tmp_export_osm_repd" AS
+SELECT match_rule, osm.objtype, osm.osm_id, osm.latitude as "osm_latitude", osm.longitude as "osm_longitude", repd.*
+	FROM matches
+		LEFT JOIN repd ON matches.master_repd_id=repd.repd_id
+		LEFT JOIN osm ON matches.master_osm_id=osm.osm_id
+	WHERE osm.repd_id_str IS NULL
+	ORDER BY repd.repd_id;
+
+\copy "tmp_export_osm_repd" TO '../data/exported/osm_repd_proposed_matches.csv' WITH DELIMITER ',' CSV HEADER;
+
+-- "Grand unified [over osm & repd] CSV of PV geolocations"
+CREATE TEMP TABLE "tmp_export_pvgeo" AS
+	SELECT repd.old_repd_id, repd.repd_id, osm.objtype as osm_objtype, osm.osm_id, repd.capacity as capacity_repd, osm.capacity as capacity_osm, repd.dev_status_short, repd.operational,
+	COALESCE(osm.latitude, repd.latitude) as latitude,
+	COALESCE(osm.longitude, repd.longitude) as longitude,
+	osm.area, osm.located, osm.orientation, osm.tag_power as osm_power_type, osm.tag_start_date as osm_tag_start_date, matches.match_rule
+	FROM matches
+		FULL JOIN repd ON (matches.master_repd_id=repd.repd_id
+			AND repd.dev_status_short NOT IN ('Abandoned', 'Application Refused', 'Application Withdrawn',  'Planning Permission Expired')
+			-- AND repd.repd_id NOT IN (1892, 1894, 6750))   -- skip three named "schemes"
+			AND match_rule NOT IN ('4', '4a'))   -- skip matches that were "schemes"
+		FULL JOIN osm ON matches.master_osm_id=osm.osm_id
+	ORDER BY repd.repd_id, osm.osm_id;
+
+\copy "tmp_export_pvgeo" TO '../data/exported/ukpvgeo_points_merged_deduped_osm-repd.csv' WITH DELIMITER ',' CSV HEADER;
+
+
