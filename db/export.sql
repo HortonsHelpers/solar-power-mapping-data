e00b66920e74ee1c77459008993c25e2b09b7df4
Dan Stowell <danstowell@users.sourceforge.net>
2020-06-25 14:49:31 +0100

    export: if osm ID is duplicated, apportion the osm capacity across duplicates.
    Similar to the existing REPD apportionment.
    Duplicate OSM IDs in the CSV can happen, e.g. for a single OSM object
    referring to two different REPD items.

--- db/export.sql
+++ db/export.sql
@@ -49,14 +49,21 @@ DELETE FROM tmp_export_pvgeo WHERE (osm_id IS NULL) AND ((repd_status IS NULL) O
 
 -- Copy in, and redistribute, the REPD official capacities: if there are multiple clusters with the same REPD ID, split the capacity equally over them. Any item that's not the cluster representative should not list the repd capacity. This way, the REPD capacity column can be meaningfully summed.
 -- (Note: NOT across items with the same cluster ID. We do not want to spread the REPD capacities over every subelement, but when there are multiple "top-level" REPD matches we have no alternative but to spread the capacity to ensure we don't double-count the capacity.)
-UPDATE tmp_export_pvgeo SET "capacity_repd_MWp"=portioned FROM (select repd_cluster_id, "capacity_repd_MWp", "capacity_repd_MWp"::float/COUNT(repd_cluster_id) as portioned from tmp_export_pvgeo where repd_cluster_id>0 and repd_id=repd_cluster_id group by repd_cluster_id, "capacity_repd_MWp") as portiontable WHERE tmp_export_pvgeo.repd_cluster_id=portiontable.repd_cluster_id and tmp_export_pvgeo.repd_cluster_id=tmp_export_pvgeo.repd_id;
+UPDATE tmp_export_pvgeo SET "capacity_repd_MWp"=portioned FROM (
+	select repd_cluster_id, "capacity_repd_MWp", "capacity_repd_MWp"::float/COUNT(repd_cluster_id) as portioned from tmp_export_pvgeo where repd_cluster_id>0 and repd_id=repd_cluster_id group by repd_cluster_id, "capacity_repd_MWp") as portiontable WHERE tmp_export_pvgeo.repd_cluster_id=portiontable.repd_cluster_id and tmp_export_pvgeo.repd_cluster_id=tmp_export_pvgeo.repd_id;
 
 UPDATE tmp_export_pvgeo SET "capacity_repd_MWp"=NULL WHERE "capacity_repd_MWp"=0; -- proper NA entries.
 
+-- We do the same for duplicated OSM IDs too.
+-- It may seem strange that we have OSM IDs duplicated, but the reason is that they can meaningfully match two REPD entries (eg a farm, plus its extension).
+-- In such a case the OSM ID appears in two rows. We don't want the osm-tagged capacity double-counted, so we subdivide it.
+UPDATE tmp_export_pvgeo SET "capacity_osm_MWp"=portioned FROM (
+	select osm_id, "capacity_osm_MWp", "capacity_osm_MWp"::float/COUNT(osm_id) as portioned from tmp_export_pvgeo where osm_id>0 and "capacity_osm_MWp">0 group by osm_id, "capacity_osm_MWp"
+) as portiontable WHERE tmp_export_pvgeo.osm_id=portiontable.osm_id;
+UPDATE tmp_export_pvgeo SET "capacity_osm_MWp"=NULL WHERE "capacity_osm_MWp"=0; -- proper NA entries.
 
 
 \copy "tmp_export_pvgeo" TO '../data/exported/ukpvgeo_points_merged_deduped_osm-repd_all.csv' WITH DELIMITER ',' CSV HEADER;
 
 \copy (SELECT * FROM tmp_export_pvgeo WHERE (osm_id = osm_cluster_id OR osm_cluster_id IS NULL OR osm_id IS NULL) AND (repd_id = repd_cluster_id OR repd_cluster_id IS NULL OR repd_id IS NULL)) TO '../data/exported/ukpvgeo_points_merged_deduped_osm-repd_clusters.csv' WITH DELIMITER ',' CSV HEADER;
-
 
